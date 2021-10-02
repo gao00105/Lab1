46b2a9ce00c75f903e7d58d3bc64e7ec973fe044
JGao <gao00105@algonquinlive.com>
2021-10-01 22:38:56 -0400

    Commit: Finished Phone Version of Lab3

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -16,43 +19,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgView;
+    Switch sw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView my_text = findViewById(R.id.textview);
-        Button my_button = findViewById(R.id.my_button);
-        EditText my_edit = findViewById(R.id.my_edittext);
-        my_button.setOnClickListener((View v) -> {
-            String editString = my_edit.getText().toString();
-            my_text.setText(getResources().getString(R.string.display_message) + editString);
-        });
-        CheckBox cat = findViewById(R.id.my_checkbox);
-        CheckBox dog = findViewById(R.id.my_checkbox2);
-        Switch gravity = findViewById(R.id.my_switch);
-        RadioButton coffee = findViewById(R.id.my_radiobtn);
-        RadioButton tea = findViewById(R.id.my_radiobtn2);
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
 
-        cat.setOnCheckedChangeListener((btn, isChecked) -> {my_text.setText("You clicked on the "+ cat.getText().toString() + " and it is now "+ isChecked);
-            Toast.makeText(getApplicationContext(), my_text.getText().toString(), Toast.LENGTH_SHORT).show();
-        });
-        dog.setOnCheckedChangeListener((btn, isChecked) -> {my_text.setText("You clicked on the "+ dog.getText().toString() + " and it is now "+ isChecked);
-            Toast.makeText(getApplicationContext(), my_text.getText().toString(), Toast.LENGTH_SHORT).show();
-        });
-        gravity.setOnCheckedChangeListener((btn, isChecked) -> {my_text.setText("You clicked on the "+ gravity.getText().toString() + " and it is now "+ isChecked);
-            Toast.makeText(getApplicationContext(), my_text.getText().toString(), Toast.LENGTH_SHORT).show();
-        });
-        coffee.setOnCheckedChangeListener((btn, isChecked) -> {my_text.setText("You clicked on the "+ coffee.getText().toString() + " and it is now "+ isChecked);
-            Toast.makeText(getApplicationContext(), my_text.getText().toString(), Toast.LENGTH_SHORT).show();
-        });
-        tea.setOnCheckedChangeListener((btn, isChecked) -> {my_text.setText("You clicked on the "+ tea.getText().toString() + " and it is now "+ isChecked);
-            Toast.makeText(getApplicationContext(), my_text.getText().toString(), Toast.LENGTH_SHORT).show();
-        });
-        ImageView my_image = findViewById(R.id.my_image);
-        ImageButton algonquin = findViewById(R.id.my_img_btn);
-        algonquin.setOnClickListener(v -> {
-            Toast.makeText(getApplicationContext(), "The width = " + algonquin.getWidth() + " and height = " + algonquin.getHeight(), Toast.LENGTH_SHORT).show();
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
         });
     }
 }
